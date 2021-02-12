0db15ce11a6e3f72ed0495e4dda247fc0ef8e271
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2021-02-12 11:50:46 -0700

    Scripts

---
+++ Scripts/Behaviors/Wander.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : SteeringBehavior
+{
+    public Kinematic character;
+    public GameObject target;
+
+    float maxAcceleration = 100f;
+
+    protected virtual Vector3 getTargetPosition()
+    {
+        return new Vector3(character.transform.position.x + Random.Range(-1, 1), character.transform.position.y, character.transform.position.z + Random.Range(-1, 1));
+    }
+
+    public override SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+        Vector3 targetPosition = getTargetPosition();
+
+        result.linear = targetPosition - character.transform.position;
+
+        // give full acceleration along this direction
+        result.linear.Normalize();
+        result.linear *= maxAcceleration;
+
+        result.angular = 0;
+        return result;
+    }
+}
+ 
---
+++ Scripts/Separater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Separater : Kinematic
+{
+    Separation myMoveType;
+    Align myAlignRotateType; //We just decided it would be this
+
+    public GameObject[] myTargets = new GameObject[4];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMoveType = new Separation();
+        myMoveType.character = this;
+        myMoveType.targets = myTargets;
+
+        myAlignRotateType = new Align();
+        myAlignRotateType.character = this;
+        myAlignRotateType.target = myTarget;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        steeringUpdate = new SteeringOutput();
+        steeringUpdate.linear = myMoveType.getSteering().linear;
+        steeringUpdate.angular = myAlignRotateType.getSteering().angular;
+        base.Update();
+    }
+}
